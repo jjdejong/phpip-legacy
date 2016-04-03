d5935e53a5e1e1159246badc795f58f81137bd7e
Jean Jacques de Jong <jjdejong@omnipat.fr>
2016-04-03 17:47:14 +0200

    Rather than redirecting to a page in case of an unauthorized access, echo a message

--- application/controllers/AuthController.php
+++ application/controllers/AuthController.php
@@ -89,6 +89,7 @@ class AuthController extends Zend_Controller_Action {
 		$this->_helper->redirector ( 'login', 'auth' );
 	}
 	public function unauthorizedAction() {
+		$this->_helper->layout->disableLayout ();
 		$this->_helper->viewRenderer->setNoRender ();
 		echo 'You are not authorized to access this resource';
 	}
