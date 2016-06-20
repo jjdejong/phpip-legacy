2a4a3d0bcb2900b96421d55a77cd98c7b32a60ac
Jean Jacques de Jong <jjdejong@omnipat.fr>
2016-06-20 11:10:24 +0200

    Fixed the last committed database upgrade script

--- install/upgrades/phpip_upgrade-current.sql
+++ install/upgrades/phpip_upgrade-current.sql
@@ -1,3 +1,4 @@
 -- This field was not used. Changing it for warning purposes, such as bad payor
 ALTER TABLE `phpip`.`actor` 
 CHANGE COLUMN `pay_category` `warn` TINYINT(1) NULL DEFAULT 0 COMMENT 'The actor will be displayed in red in the matter view when set' ;
+UPDATE actor SET warn=0 WHERE warn=1;
