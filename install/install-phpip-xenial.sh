531a5be09664bcb2da39c53c8518790984910242
Jean Jacques de Jong <jjdejong@omnipat.fr>
2017-12-03 15:46:19 +0100

    Fixed the install script to account for a location change of the Zend libraries

--- install/install-phpip-xenial.sh
+++ install/install-phpip-xenial.sh
@@ -17,6 +17,8 @@ sed -i "s/^#application\/x-httpd-php/application\/x-httpd-php/" /etc/mime.types
 a2enmod rewrite
 echo "127.0.0.1    phpip.local" >> /etc/hosts
 sed -i "s/^; include/include/" /etc/php/7.0/mods-available/zend-framework.ini
+# This corrects a bug in the stock zend-framework.ini file
+sed -i "s/php\/libzend-framework-php/php/" /etc/php/7.0/mods-available/zend-framework.ini
 phpenmod zend-framework
 echo "
 ********************************
@@ -33,3 +35,7 @@ Installing database.
 When prompted for a password, enter the MySQL password defined earlier
 ********************************"
 mysql -u root -p < phpip/install/phpip_skel-current.sql
+echo "
+Ready to go now. 
+Point your browser to http://phpip.local and login with phpipuser:changeme
+Report any issues on GitHub: https://github.com/jjdejong/phpip/issues"
